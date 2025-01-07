42b657e65131d6b1d94316147914bd65bbecce37
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2025-01-07 09:50:22 +0200

    adding colors constants

---
+++ constants/Colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  primaryGreen: '#003324',
+  secondaryGreen: '#8BC652',
+  orange: '#F76711',
+  eggShellLight: '#F5EBE2',
+  eggShellDark: '#F6E5D5',
+  primeGreen5: '#0033240D',
+  primeGreen50: '#00332480',
+  secondGreen20: '#8BC65233',
+  secondGreen40: '#8BC65266',
+}
+
+export default colors
